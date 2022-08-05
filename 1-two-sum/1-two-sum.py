7d9ee0a73c87bf38e9c9509b28f0c6c3454b0947
Hyunmin <76608229+nimnuyh@users.noreply.github.com>
2022-08-05 09:50:54 +0900

    Time: 703 ms (46.58%), Space: 14 MB (94.80%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,6 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)) :
-            for j in range(i + 1, len(nums)) :
-                    if nums[i] + nums[j] == target :
-                        return [i, j]
-                
+class Solution:
+    def twoSum(self, nums, target) :
+        pairs = [target - num for num in nums]
+        for i, pair in enumerate(pairs):
+            if pair in nums and i != nums.index(pair):
+                return [i, nums.index(pair)]
