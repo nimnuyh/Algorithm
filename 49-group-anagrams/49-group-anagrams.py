32c98d2340339725ebacd4004f1a69ab49e91869
Hyunmin <76608229+nimnuyh@users.noreply.github.com>
2022-08-05 10:55:59 +0900

    Time: 71 ms (99.25%), Space: 17.3 MB (90.01%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,14 @@
+import collections
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        sorted_list = []
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return anagrams.values()
