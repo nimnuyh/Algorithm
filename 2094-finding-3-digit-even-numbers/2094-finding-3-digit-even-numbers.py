9be390ad571c53c57ff490e859eed0a89fc3c4e0
Hyunmin <76608229+nimnuyh@users.noreply.github.com>
2022-08-15 13:46:34 +0900

    Time: 185 ms (55.95%), Space: 13.5 MB (39.29%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -4,15 +4,26 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        First = [i for i, digit in enumerate(digits) if digit != 0]
-        Second = list(range(0, len(digits)))
-        Last = [i for i, digit in enumerate(digits) if digit %2 == 0]
-        Answer = set()
-        for i in First:
-            for j in Second:
-                for k in Last:
-                    if i != j and i != k and j != k:
-                        Answer.add(digits[i]*100 + digits[j]*10 + digits[k])
-        Answer = list(Answer)
-        Answer.sort()
-        return Answer
+        digit_dict = {}
+        re_digits = []
+        for d in digits :
+            check = digit_dict.get(d)
+            if check == None :
+                digit_dict[d] = 1
+                re_digits.append(d)
+            elif check < 3 :
+                digit_dict[d] += 1
+                re_digits.append(d)
+        
+        ans = set()
+        first = [i for i, v in enumerate(re_digits) if v != 0]
+        second = list(range(len(re_digits)))
+        third = [i for i, v in enumerate(re_digits) if v % 2 == 0]
+        for x in first :
+            for y in second :
+                for z in third :
+                    if (x != y) and (y != z) and (z != x) :
+                        ans.add((re_digits[x] * 100) + (re_digits[y] * 10) + re_digits[z])
+        ans = list(ans)
+        ans.sort()
+        return ans
