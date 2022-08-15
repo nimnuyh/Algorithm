f2cb7b800d6a5d3398ee7e46d7b56f2f9b3d61a7
Hyunmin <76608229+nimnuyh@users.noreply.github.com>
2022-08-15 21:32:30 +0900

    Time: 50 ms (95.38%), Space: 13.4 MB (52.02%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.py
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -6,15 +6,9 @@ class Solution(object):
         :type nums3: List[int]
         :rtype: List[int]
         """
-        num = nums1 + nums2 + nums3
-        num_list = [nums1, nums2, nums3]
-        num_set = set(num)
-        a = []
-        for i in num_set :
-            c = 0
-            for j in num_list :
-                if j.count(i) > 0 :
-                    c += 1
-            if c > 1 :
-                a.append(i)
-        return a
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        nums3 = set(nums3)
+        
+        ans = list((nums1 & nums2) | (nums2 & nums3) | (nums3 & nums1))
+        return ans
