9d6fe49bd49fcf47e228e87385929e28e651de06
Hyunmin <76608229+nimnuyh@users.noreply.github.com>
2022-08-17 19:27:43 +0900

    [D1] Title: 1대1 가위바위보, Time: 164 ms, Memory: 58,524 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,13 @@
+# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+# 아래 표준 입출력 예제 필요시 참고하세요.
+
+x, y = map(int, input().split())
+
+if x == 1 and y == 3 :
+    print('A')
+elif x == 2 and y == 1 : 
+    print('A')
+elif x == 3 and y == 2 :
+    print('A')
+else :
+    print('B')
