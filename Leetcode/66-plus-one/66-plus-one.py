b25679dff53a852def375cf114e8c5391a57d3cc
nimnuyh <khm950903@gmail.com>
2022-08-17 11:29:20 +0900

    edit

--- Leetcode/66-plus-one/66-plus-one.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        return list(str(int(''.join([str(x) for x in digits])) + 1))
